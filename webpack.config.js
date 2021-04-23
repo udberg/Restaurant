0de3f3dcab662270f5cd2c991833be83f98b64e3
udberg <johnuduak066@gmail.com>
2021-04-23 22:52:21 +0100

    Fix linters errors

--- webpack.config.js
+++ webpack.config.js
@@ -7,19 +7,19 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader",
+          'style-loader,
+          'css-loader',
+          'sass-loader',
         ],
 	  },
 	  {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        use: ['file-loader']
-      }
-    ]
+        use: ['file-loader'],
+      },
+    ],
   },
   devtool: 'source-map',
   devServer: {
-    contentBase: './dist'
-  }
+    contentBase: './dist',
+  },
 };
